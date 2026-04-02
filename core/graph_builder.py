dde1ec86b002c650ca40e8a17aacadff357f4c77
imtrum <superman2019xd@gmail.com>
2026-04-02 14:46:28 +0700

    Update graph_builder.py

--- core/graph_builder.py
+++ core/graph_builder.py
@@ -1,15 +1,16 @@
 def build_graph(stop_times):
     """
     Build graph: stop -> list of neighbors
+    danh sách stop và đỉnh kề
     """
     graph = {}
 
-    # group by trip
+    # nhóm vào những dòng dữ liệu cùng trip_id
     trips = {}
     for trip_id, stop_id, arr, dep, seq in stop_times:
         trips.setdefault(trip_id, []).append((seq, stop_id, dep))
 
-    # sort each trip by sequence
+    # xếp theo thứ tự đầu cuối quãng đường nhờ thuộc tính sequence
     for trip_id in trips:
         trips[trip_id].sort()
 
@@ -17,6 +18,6 @@ def build_graph(stop_times):
             _, current_stop, dep_time = trips[trip_id][i]
             _, next_stop, next_dep = trips[trip_id][i+1]
 
-            graph.setdefault(current_stop, []).append((next_stop, dep_time))
+            graph.setdefault(current_stop, []).append((next_stop, dep_time)) #1 cạnh của 1 node gồm thời gian xuất phát và điểm đến tiếp theo
 
     return graph
