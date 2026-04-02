f1d2b8a3e7b1ff79ccd0996adcea1033c827bd47
imtrum <superman2019xd@gmail.com>
2026-04-02 22:49:45 +0700

    Update graph_builder.py

--- core/graph_builder.py
+++ core/graph_builder.py
@@ -1,16 +1,16 @@
+from utils.time_utils import time_to_seconds
 def build_graph(stop_times):
     """
     Build graph: stop -> list of neighbors
-    danh sách stop và đỉnh kề
     """
     graph = {}
 
-    # nhóm vào những dòng dữ liệu cùng trip_id
+    # group by trip
     trips = {}
     for trip_id, stop_id, arr, dep, seq in stop_times:
         trips.setdefault(trip_id, []).append((seq, stop_id, dep))
 
-    # xếp theo thứ tự đầu cuối quãng đường nhờ thuộc tính sequence
+    # sort each trip by sequence
     for trip_id in trips:
         trips[trip_id].sort()
 
@@ -18,6 +18,10 @@ def build_graph(stop_times):
             _, current_stop, dep_time = trips[trip_id][i]
             _, next_stop, next_dep = trips[trip_id][i+1]
 
-            graph.setdefault(current_stop, []).append((next_stop, dep_time)) #1 cạnh của 1 node gồm thời gian xuất phát và điểm đến tiếp theo
+            graph.setdefault(current_stop, []).append((next_stop, dep_time, next_dep))
+    
+    for edges in graph.values():
+        edges.sort(key=lambda edge: time_to_seconds(edge[1]))
+        
 
     return graph
