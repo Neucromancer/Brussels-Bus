443f5123bf1670f094d8f535ae017e199b391884
Neuromancer <Nguyenminhdung1324@gmail.com>
2026-04-02 09:08:10 +0700

    Revert "Initial commit (clean)"
    This reverts commit a8949e94e9ca65743f09f566711236a070570c76.

--- core/routing_utils.py
+++
@@ -1,5 +0,0 @@
-from utils.time_utils import time_to_seconds
-
-def get_next_departure(current_time, departure_time):
-    dep = time_to_seconds(departure_time)
-    return dep if dep >= current_time else None
