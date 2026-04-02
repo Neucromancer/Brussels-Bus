0e0678d325a015ab8a7e1fada91e95fd31ef1952
imtrum <superman2019xd@gmail.com>
2026-04-02 23:30:53 +0700

    Update data_loader.py

--- core/data_loader.py
+++ core/data_loader.py
@@ -1,5 +1,5 @@
 import sqlite3
-
+# lấy bảng stop_times từ database
 def load_stop_times(db_path):
     conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
@@ -10,32 +10,28 @@ def load_stop_times(db_path):
     conn.close()
     return rows
 
-
+#lấy tọa độ các stop xuát hiện trong bảng stop_times
 def load_coordinates_for_used_stops(db_path, stop_times):
-    """
-    Chỉ lấy những điểm dừng có trong stop_times
-    """
-    conn = sqlite3.connect(db_path)
-    cursor = conn.cursor()
-
-    # lấy những stopid có trong stop_times, lọc trùng nhau 
-    stop_ids = list({row[1] for row in stop_times})
-
-    # build SQL IN clause safely
-       #tham khảo AI
-    placeholders = ",".join(["?"] * len(stop_ids))  #tạo chuỗi "?,?,...." độ dài bằng số stop_ids
-    query = f"SELECT stop_id, stop_lat, stop_lon FROM stops WHERE stop_id IN ({placeholders})"  
-    #tạo câu truy vấn
+  
 
 
     
-    #lấy stopid, tọa độ của stopid 
-    cursor.execute(query, stop_ids)   # python sẽ thay chuỗi "?" bằng dữ liệu thật
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+
+    # lấy những stop_id xuất hiện trong stop_times
+    stop_ids = list({row[1] for row in stop_times})
+
+    # xấy dựng câu truy vấn và lấy tọa độ từ stops table(tham khảo AI)
+    placeholders = ",".join(["?"] * len(stop_ids)) 
+    query = f"SELECT stop_id, stop_lat, stop_lon FROM stops WHERE stop_id IN ({placeholders})"
+
+    cursor.execute(query, stop_ids)
     rows = cursor.fetchall()
 
     conn.close()
 
-    # tạo danh sách stop_ids và tọa độ:  stop_id -> (lat, lon)
+    # xây dựng bảng toạ độ :stop_id -> (lat, lon)
     coordinates = {stop_id: (float(lat),float(lon)) for stop_id, lat, lon in rows}
 
     return coordinates
