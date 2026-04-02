5bb5e1ad901aff88c4cb069170ec6d07f83a39e7
imtrum <superman2019xd@gmail.com>
2026-04-02 14:40:15 +0700

    Update data_loader.py

--- core/data_loader.py
+++ core/data_loader.py
@@ -13,24 +13,29 @@ def load_stop_times(db_path):
 
 def load_coordinates_for_used_stops(db_path, stop_times):
     """
-    Load ONLY coordinates for stops that appear in stop_times
+    Chỉ lấy những điểm dừng có trong stop_times
     """
     conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
 
-    # extract unique stop_ids from stop_times
+    # lấy những stopid có trong stop_times, lọc trùng nhau 
     stop_ids = list({row[1] for row in stop_times})
 
     # build SQL IN clause safely
-    placeholders = ",".join(["?"] * len(stop_ids))
-    query = f"SELECT stop_id, stop_lat, stop_lon FROM stops WHERE stop_id IN ({placeholders})"
+       #tham khảo AI
+    placeholders = ",".join(["?"] * len(stop_ids))  #tạo chuỗi "?,?,...." độ dài bằng số stop_ids
+    query = f"SELECT stop_id, stop_lat, stop_lon FROM stops WHERE stop_id IN ({placeholders})"  
+    #tạo câu truy vấn
 
-    cursor.execute(query, stop_ids)
+
+    
+    #lấy stopid, tọa độ của stopid 
+    cursor.execute(query, stop_ids)   # python sẽ thay chuỗi "?" bằng dữ liệu thật
     rows = cursor.fetchall()
 
     conn.close()
 
-    # build dict: stop_id -> (lat, lon)
+    # tạo danh sách stop_ids và tọa độ:  stop_id -> (lat, lon)
     coordinates = {stop_id: (float(lat),float(lon)) for stop_id, lat, lon in rows}
 
     return coordinates
